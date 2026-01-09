9c86dcd4f98803c7c1021cc92a0e71123a4a3200
aindrigo <94946723+aindrigo@users.noreply.github.com>
2026-01-08 21:29:13 -0500

    license

--- lua/aerial/hooks/sh_hooks.lua
+++ lua/aerial/hooks/sh_hooks.lua
@@ -1,3 +1,21 @@
+--[[
+    Aerial, a weapon base designed to ease the creation of realistic weapons within Garry's Mod.
+    Copyright (C) 2026  aindrigo
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]--
+
 hook.Add("PlayerButtonDown", "aerialButtonDown", function(ply, button)
     local code = ply:GetInfoNum("aerial_bind_firemode", 0)
     if code < 1 or button ~= code then return end
--- lua/weapons/aerial_base/shared/camera.lua
+++ lua/weapons/aerial_base/shared/camera.lua
@@ -1,3 +1,21 @@
+--[[
+    Aerial, a weapon base designed to ease the creation of realistic weapons within Garry's Mod.
+    Copyright (C) 2026  aindrigo
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]--
+
 function SWEP:CalculateFOV()
     if istable(self.ADS) and isnumber(self.ADS.FOV) and self:GetADS() then
         return self.ADS.FOV
